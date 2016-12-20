e2994ef87ecc087dcbeaa4a8886e8800ef764e27
S Percentage <Syn.Tri.Naga@gmail.com>
2016-12-20 18:06:06 +0900

    added player motion and clipping

---
+++ Assets/PlayerMotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 3.0f;
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		var tf = this.transform.position;
+		tf += Vector3.up * Input.GetAxis("Vertical") * this.speed * Time.deltaTime;
+		tf.y = Mathf.Clamp(tf.y, -5.0f + this.transform.localScale.y * 5.0f, 5.0f - this.transform.localScale.y * 5.0f);
+		this.transform.position = tf;
+	}
+}
