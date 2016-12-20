725e2f8ea26d80ef1fdfd7c6fc7c0b8a75ff1629
S Percentage <Syn.Tri.Naga@gmail.com>
2016-12-20 18:20:51 +0900

    added arrow

---
+++ Assets/ArrowMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMotion : MonoBehaviour
+{
+	public Vector3 internalMotion = new Vector3(16.0f, 0.0f, 0.0f);
+	[SerializeField]
+	float gravityMagnitude = 9.82f;
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		this.internalMotion += Vector3.down * 9.82f * Time.deltaTime;
+		this.transform.localRotation = Quaternion.AngleAxis(Mathf.Atan2(this.internalMotion.y, this.internalMotion.x) * Mathf.Rad2Deg, Vector3.forward)
+			* Quaternion.AngleAxis(90.0f, Vector3.left);
+		this.transform.position = this.transform.position + this.internalMotion * Time.deltaTime;
+	}
+}
