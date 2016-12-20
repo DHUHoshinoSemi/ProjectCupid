d71ee3c9b0061ad6249017bfa412482c67776839
S Percentage <Syn.Tri.Naga@gmail.com>
2016-12-20 18:25:46 +0900

    added shooting script

---
+++ Assets/DoFire.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoFire : MonoBehaviour
+{
+	[SerializeField]
+	GameObject arrowTemplate;
+
+	void Update ()
+	{
+		if (Input.GetButtonDown("Fire"))
+		{
+			var go = GameObject.Instantiate(this.arrowTemplate, this.transform.position, Quaternion.identity).GetComponent<ArrowMotion>();
+			go.internalMotion = new Vector3(16.0f, 4.0f, 0.0f);
+		}
+	}
+}
