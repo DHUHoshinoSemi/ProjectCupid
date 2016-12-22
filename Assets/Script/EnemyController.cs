056deaa2d907408e810992a4cda18ee73141076e
Taiga Maki <a14dc152@dhw.ac.jp>
2016-12-22 14:42:56 +0900

    ハート呼び出しコルチン実装

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -2,36 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
 
-    Rigidbody2D rb;
-    int HeartRandom = Random.Range(1, 6);
-    int i = 0;
-    GameObject Enemy;
-
-    public int moveSpeed = 0;
+    public float moveSpeed = 0;
     public GameObject Heart;
 
+    float y = -4;
 
+    Rigidbody2D rb;
+    int HeartRandom = Random.Range(2, 6);
+    int i = 0;
+    
+    GameObject Enemy;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Debug.Log(HeartRandom);
 
-        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("heartAppear");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        transform.position = v;
+    }
+
+    private IEnumerator heartAppear()
+    {
         Enemy = GameObject.Find("Enemy");
         Vector2 pos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 2);
 
         for (i = 0; i < HeartRandom; i++)
         {
-            //Instantiate(Heart);
+            float waitTime = Random.Range(1.5f, 5.0f);
+            Debug.Log(waitTime);
             Heart = (GameObject)Instantiate(Heart, transform.position, Quaternion.identity);
             Heart.transform.parent = Enemy.transform;
+            yield return new WaitForSeconds(waitTime);
         }
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
     }
 }
--- Assets/Script/HeartController.cs
+++ Assets/Script/HeartController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class HeartController : MonoBehaviour {
 
     Rigidbody2D rb;
-    public int moveSpeed = 0;
+
+    public float upSpeed = 0;
+
+    private Vector2 m_pos;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        m_pos = transform.localPosition;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        transform.localPosition = m_pos;
+        m_pos.y += upSpeed;
     }
 }
