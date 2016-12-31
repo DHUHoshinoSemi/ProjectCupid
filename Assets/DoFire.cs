e86a0c9b68a95e2e6c817f45d3fb6e937dface00
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2016-12-31 15:40:22 +0900

    applied bow power to arrow's initial velocity

--- Assets/DoFire.cs
+++ Assets/DoFire.cs
@@ -10,16 +10,14 @@ public class DoFire : MonoBehaviour
 	[SerializeField]
 	UnityEngine.UI.Image reactionTarget;
 
-	public float BowStrength { get { return this.bowStrength; } }
-
 	void Update ()
 	{
+		this.bowStrength = Mathf.Clamp01(this.bowStrength + Input.GetAxis("Horizontal") * Time.deltaTime);
+		this.reactionTarget.rectTransform.sizeDelta = new Vector2(this.reactionTarget.rectTransform.sizeDelta.x, this.bowStrength * 128.0f);
 		if (Input.GetButtonDown("Fire"))
 		{
 			var go = GameObject.Instantiate(this.arrowTemplate, this.transform.position, Quaternion.identity).GetComponent<ArrowMotion>();
-			go.internalMotion = new Vector3(16.0f, 4.0f, 0.0f);
+			go.internalMotion = new Vector3(2.0f, 0.0f, 0.0f) + new Vector3(14.0f, 4.0f, 0.0f) * this.bowStrength;
 		}
-		this.bowStrength = Mathf.Clamp01(this.bowStrength + Input.GetAxis("Horizontal") * Time.deltaTime);
-		this.reactionTarget.rectTransform.sizeDelta = new Vector2(this.reactionTarget.rectTransform.sizeDelta.x, this.bowStrength * 128.0f);
 	}
 }
