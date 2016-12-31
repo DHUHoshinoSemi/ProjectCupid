7c2707ccfa8077a68f53f0625713b2ccfc714efa
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2016-12-31 16:04:32 +0900

    added ArrowDispose

---
+++ Assets/ArrowDispose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDispose : MonoBehaviour
+{
+	void Update()
+	{
+		if(this.transform.position.x >= Camera.main.orthographicSize * Camera.main.aspect ||
+			this.transform.position.y < -Camera.main.orthographicSize)
+		{
+			GameObject.Destroy(this.gameObject, 0.25f);
+		}
+	}
+}
